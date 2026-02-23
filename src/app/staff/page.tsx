f0058e817a2f7f7752dabc1a015b09ae7405e4a1
phonlawat <phonlawat.chi@gmail.com>
2026-02-23 13:59:32 +0700

    initia

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -113,8 +113,6 @@ export default function StaffPage() {
     };
   }, [filteredSubmissions]);
 
-  console.log(filteredSubmissions);
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
